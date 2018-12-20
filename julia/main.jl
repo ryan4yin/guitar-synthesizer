f3aa3be6a0de5b05ca279fa37d850de27d93ed79
ryan4yin <xiaoyin_c@qq.com>
2018-12-20 15:11:21 +0800

    add implementation in julia

---
+++ julia/main.jl
@@ -0,0 +1,74 @@
+function makewave(f, t, s, Fs)
+    # f : 频率
+    # t : 时长
+    # s: 最大振幅
+    # Fs: 采样频率
+
+    N = ceil(Int, Fs*t); # 总的采样点个数
+    L = floor(Int, Fs/f); # 一个周期所含的采样数(整)
+    part = rand(L, 1) .- 0.5; # 循环段
+    np = floor(Int, N/L); # 总的循环次数
+
+    r = 0.5; # 平均权值
+
+    wave=zeros(N, 1);
+    for i = 1:np
+        part = part .* r + [part[end]; part[1:end-1]] .* (1 - r);   # 吉他音质算法
+        pos = L * (i -1) + 1;
+        wave[pos:pos+L-1] = part;
+    end
+
+    wave = wave .* range(1, stop=0, length=length(wave));
+
+    wave * s
+end
+
+
+
+
+# 需要用到第三方模块：WAV
+using WAV
+
+Fs = 44100;
+a1 = 440;  #标准音
+r = 2^(1/12); #12平均律的比值
+t = .5; #1拍的时长
+
+#主旋律 〈八辈子〉
+song = [3 1; 5 1; 3 1; 3 .5; 5 .5; 10 1; 10 1; 10 .5; 7 .5; 5 .5; 7 .5; 5 1; 3 1; 3 1;
+   3 .5; 5 .5; 5 1; 3 1; 3 1; -2 .5; -2 .5;
+];
+#伴奏    〈八辈子〉
+chords = [3 -2 3 5 7 5 10 7 3 -2 3 5 7 5 10 7 -2 -2 3 5 7 5 10 7 -2 -2 3 5 7 5 7 10]';
+
+
+beats = sum(song[:,2])
+
+l = Int(beats * 2); # 伴奏每个音符都只有半拍，所以长度乘个二
+while length(chords) < l
+    chords = [chords; chords];
+end
+chords = chords[1:l];
+
+wave =[];
+for i = 1:size(song, 1)
+    f = a1 * r^song[i,1];
+    f_t = t * song[i,2];
+
+    tone = makewave(f, f_t, 1, Fs);
+    wave = isempty(wave) ? tone : [wave; tone];
+end
+
+wave_c = [];
+for i = 1:size(chords, 1)
+    f = a1 * r^chords[i];
+    f_t = t * .5;  # 伴奏每个音符都只有半拍
+
+    tone = makewave(f, f_t, .3, Fs)
+    wave_c = isempty(wave_c) ? tone : [wave_c; tone];
+end
+
+ll = min(length(wave), length(wave_c))
+
+wav = wave[1:ll] + wave_c[1:ll]
+wavplay(wav, Fs)
